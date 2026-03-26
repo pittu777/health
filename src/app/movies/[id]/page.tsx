03d43f62d94880ed1eb213374aec45167d17a8a3
pittu777 <pittuprasanth98@gmail.com>
2026-03-26 17:14:26 +0530

    "deleted"

--- src/app/movies/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/movies/[id]/page.tsx
-import MovieDetailFeature from "@/feature/movies/[id]/page";
-
-export default async function Page({ params }: { params: Promise<{ id: string }> }) {
-    // Await the params to get the actual ID value
-    const resolvedParams = await params;
-
-    return <MovieDetailFeature params={resolvedParams} />;
-}
